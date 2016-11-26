4379eaf43da36140dd1630a5555c71a87f1bf663
Honey Lakhani <honey.lakhani@payu.in>
2016-11-26 16:42:02 +0530

    first commit

---
+++ MPOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MPOS
+//
+//  Created by Honey Lakhani on 11/26/16.
+//  Copyright © 2016 com.payu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MPOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MPOS
+//
+//  Created by Honey Lakhani on 11/26/16.
+//  Copyright © 2016 com.payu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
